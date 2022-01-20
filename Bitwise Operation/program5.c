4bc1a11e5e82d105952fad6f98b1fcaeb21727fb
mdFahimMorshed <contactahmfahim@gmail.com>
2022-01-20 15:51:20 +0600

    bitwise

---
+++ Bitwise Operation/program5.c
@@ -0,0 +1,18 @@
+/*
+    5 = 0101
+    6 = 0110
+  5^6 = 0011 = 3  
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int n1 = 5, n2 = 6, n3;
+
+    n3 = n1 ^ n2;
+
+    printf("%d ^ %d = %d\n", n1, n2, n3);
+
+    return 0;
+}
---
+++ Bitwise Operation/program8.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+char to_upper(char ch)
+{
+    return ch & 95;
+}
+
+char to_lower(char ch)
+{
+    return ch | 32;
+}
+
+int main()
+{
+    char *str = "AbCdEfGhIjKlMnOpQrStUvWxYz";
+
+    for (int i = 0; i < 26; i++)
+    {
+        printf("Uppercase : %c, ", to_upper(str[i]));
+        printf("lowercase : %c\n", to_lower(str[i]));
+    }
+
+    return 0;
+}
