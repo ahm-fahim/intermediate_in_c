fc029281e2ec68b56d8fa2fd02baf343dc29b67f
mdFahimMorshed <contactahmfahim@gmail.com>
2022-01-25 11:31:57 +0600

    Pointer with dynamic memory allocation

---
+++ More Pointer/Dynamic Memory Allocation/program3.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int *ara[5], num[5];
+    int i, j, n;
+
+    for (i = 0; i < 5; i++)
+    {
+        printf("Enter the number of students for class %d: ", i + 1);
+
+        scanf("%d", &n);
+        num[i] = n;
+        ara[i] = (int *)malloc(sizeof(int) * n);
+        if (ara[i] == NULL)
+        {
+            printf("Memory allocation failed\n");
+            return 1;
+        }
+        for (j = 0; j < n; j++)
+        {
+            printf("Enter marks for stduent %d: ", j + 1);
+            scanf("%d", &ara[i][j]);
+        }
+    }
+
+    // now print the result
+    printf("Output\n\n");
+
+    for (i = 0; i < 5; i++)
+    {
+        printf("Class %d: ", i + 1);
+        for (j = 0; j < num[i]; j++)
+        {
+            printf("%4d", ara[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
