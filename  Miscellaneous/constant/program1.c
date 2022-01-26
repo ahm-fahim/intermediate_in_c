ad5def2a011751d14533d1c31ce72d199195f300
mdFahimMorshed <contactahmfahim@gmail.com>
2022-01-26 16:04:06 +0600

    constant

---  Miscellaneous/constant/program1.c
+++  Miscellaneous/constant/program1.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <math.h>
 
+//preprocessor
+#define MIN -1
+#define PI (2 * acos(0))
+
 int main()
 {
-    printf("Hello world!");
+    printf("value of MIN: %d\n",MIN);
+    printf("value of PI: %lf\n", PI);
+
     return 0;
 }
